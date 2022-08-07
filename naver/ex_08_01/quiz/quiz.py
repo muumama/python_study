fb8ff790b2a248eaf44a3bfd2d9ae4e6ffa1da9e
muumama <dufi212@naver.com>
2022-08-07 19:35:36 +0900

    a

---
+++ naver/ex_08_01/quiz/quiz.py
@@ -0,0 +1,5 @@
+asia = ['korea', 'japan', 'china']
+america = ['usa', 'canada', 'mexico']
+
+asia.append(america[0])
+print(asia)
---
+++ naver/ex_08_01/quiz/quiz2.py
@@ -0,0 +1,5 @@
+num1 = [1, 2, 3, 4]
+num2 = [6, 7, 8, 9]
+num1.append(num2)
+len(num1)
+print(num1)
