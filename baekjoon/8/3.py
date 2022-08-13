ed5d843b135491262bc1d363d6d63bb02e86244d
muumama <dufi212@naver.com>
2022-08-13 17:27:34 +0900

    code: update sys

--- baekjoon/8/3.py
+++ baekjoon/8/3.py
@@ -1,8 +1,12 @@
-h,m = map(int,input().split())
+import sys
+
+
+h,m = map(int,sys.stdin.readline().split())
 p = int(input())
 
 if m + p >= 60:
-    h = h + (m+p)/60
+#    h = h + (m+p)/60
+    h += (m+p)/60
 if h >= 24:
     h = h-24
 
