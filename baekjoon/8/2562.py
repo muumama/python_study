81d22eca59d0a4731be2024631a5b1685da8e528
muumama <dufi212@naver.com>
2022-08-19 21:45:03 +0900

    code_num

---
+++ baekjoon/8/2562.py
@@ -0,0 +1,18 @@
+#2562
+a = [1,1,1,1,1,1,1,1]
+print(len(a))
+'''
+num = []
+for i in range(9):
+  num.append(input())
+
+n = 0
+b = num[0]
+for i in range(len(num)):
+  if b <= num[n]:
+      b = num[n]
+  n = n + 1
+
+print(b,
+      n-1)
+'''
