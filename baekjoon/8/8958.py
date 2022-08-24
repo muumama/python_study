9735e5b2d2d6299536113f14c00d9d1591a30ba1
muumama <dufi212@naver.com>
2022-08-24 23:21:20 +0900

    baekjoon_ox

--- baekjoon/8/8958.py
+++ baekjoon/8/8958.py
@@ -1,29 +1,24 @@
 #8958
 
 a = int(input())
-
-b = []
+score_list = []
 
 for i in range(a):
-    b += input()
+    b = input()
 
     score = 0
-    n = 0
     score_plus = 0
-    score_list = []
-    for j in range(len(b[i])):    
-        
-        if b[i][j] == "O":
-            if b[i][j] == b[i][n]:
-                score_plus = score_plus + 1
-            else:
-                score_plus = 1
+
+    for j in b:    
+        if j == "O":
+            score_plus += 1
+            score += score_plus
         else:
             score_plus = 0
-        score = score + score_plus
-        n = j - 1
+    #print(score)
     score_list.append(score)
-print(score_list)
+    
+print(*score_list, sep='\n')
                     
                 
                 
