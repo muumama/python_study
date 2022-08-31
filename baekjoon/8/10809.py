7232f5b6cadfb0d06b1bc5cbb78141532d3d7886
muumama <dufi212@naver.com>
2022-08-31 19:27:21 +0900

    code_num

---
+++ baekjoon/8/10809.py
@@ -0,0 +1,5 @@
+#10809
+s = input()
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+for i in alphabet:
+  print(s.find(i), end =' ')
---
+++ baekjoon/8/11720.py
@@ -0,0 +1,8 @@
+#11720
+n = int(input())
+num = input()
+num_sum = 0
+for i in range(n):
+  num_sum += int(num[i])
+
+print(num_sum)
