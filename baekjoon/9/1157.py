1216e2fac057d592546d31e82df60520364a7cf0
muumama <dufi212@naver.com>
2022-09-04 19:44:39 +0900

    code_set_max

--- baekjoon/9/1157.py
+++ baekjoon/9/1157.py
@@ -1,14 +1,19 @@
-#1157
-words = input().upper()
-unique_words = list(set(words))  # 입력받은 문자열에서 중복값을 제거
+
+word = input().upper()
+word_uni = list(set(word))
+
 
 cnt_list = []
-for x in unique_words :
-    cnt = words.count(x)
-    cnt_list.append(cnt)  # count 숫자를 리스트에 append
-
-if cnt_list.count(max(cnt_list)) > 1 :  # count 숫자 최대값이 중복되면
+for i in word_uni:
+    cnt = word.count(i)
+    cnt_list += cnt
+    
+if cnt_list.count(max(cnt_list)) > 1:
     print('?')
 else :
-    max_index = cnt_list.index(max(cnt_list))  # count 숫자 최대값 인덱스(위치)
-    print(unique_words[max_index])
+    max_index = cnt_list.index(max(cnt_list))
+    print(word_uni[max_index])
+    
+    
+
+
