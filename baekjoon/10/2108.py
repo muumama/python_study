9cae27d657643ee5143757878d22bbf2da68e611
muumama <110667661+muumama@users.noreply.github.com>
2022-10-05 17:15:39 +0900

    Update 2108.py
    Counter

--- baekjoon/10/2108.py
+++ baekjoon/10/2108.py
@@ -1,16 +1,35 @@
 #2108
-import numpy
-n = int(input())
+import sys
+from collections import Counter
+
+n = int(sys.stdin.readline())
 num_list = []
 for i in range(n):
-  num_list += int(input())
+  num_list += [int(sys.stdin.readline())]
 num_list.sort()
-print(numpy.mean(num_list))
+
+#산술평균
+a = int(sum(num_list))
+if a >= 0 :
+  print(int(sum(num_list) / n + 0.5))
+elif a < 0 :
+  print(int(sum(num_list) / n - 0.5))
+
+#중앙값
 print(num_list[n//2])
 
 #최빈값
-print()
+cnt = Counter(num_list)
+tmp = cnt.most_common()
 
+
+if len(tmp) > 1:
+  if tmp[0][1] == tmp[1][1]:
+    print(tmp[1][0])
+  else:
+    print(tmp[0][0])
+else:
+  print(tmp[0][0])
 
 #범위
 print(max(num_list)-min(num_list))
