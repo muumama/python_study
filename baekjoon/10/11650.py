86404da0f1f2dcd99c00fbf66575c4c766195ee8
muumama <dufi212@naver.com>
2022-10-09 22:46:11 +0900

    dict

---
+++ baekjoon/10/11650.py
@@ -0,0 +1,9 @@
+import sys
+n = int(sys.stdin.readline())
+array = []
+for i in range(n):
+    [a,b] = map(int, sys.stdin.readline().split())
+    array.append([a,b])
+s_array = sorted(array)
+for i in range(n):
+    print(s_array[i][0], s_array[i][1])
