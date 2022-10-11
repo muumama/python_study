36a6a1628a774306c903d492ebe46aa93b95dbe3
muumama <dufi212@naver.com>
2022-10-11 19:46:04 +0900

    count

---
+++ baekjoon/10/11651.py
@@ -0,0 +1,9 @@
+import sys
+n = int(sys.stdin.readline())
+array = []
+for i in range(n):
+    [a,b] = map(int, sys.stdin.readline().split())
+    array.append([b,a])
+s_array = sorted(array)
+for i in range(n):
+    print(s_array[i][1], s_array[i][0])
