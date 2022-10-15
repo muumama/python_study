9010cf646e339d92059c60d5e1d9b16c3fcfbf69
muumama <dufi212@naver.com>
2022-10-15 20:52:14 +0900

    c

--- baekjoon/10/25305.py
+++ baekjoon/10/25305.py
@@ -1,6 +1,6 @@
 #25305 baekjoon
 import sys
-n, k = map(int, sys.stdin.readline().split())
+n, k =  map(int, sys.stdin.readline().split())
 score = list(map(int, sys.stdin.readline().split()))
 score.sort(reverse=True)
 print(score[k-1])
