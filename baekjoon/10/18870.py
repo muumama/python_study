0762d808b28129d5dd757a64858bc619b0e55a56
muumama <dufi212@naver.com>
2022-10-17 20:09:06 +0900

    sort

--- baekjoon/10/18870.py
+++ baekjoon/10/18870.py
@@ -1,13 +1,15 @@
 #18870
 import sys
-n = int(intput())
-numbers = map(int, sys.stdin.readline().split())
+n = int(input())
+numbers = list(map(int, sys.stdin.readline().rstrip().split()))
+#print(numbers)
 num_set = set(numbers)
 num_list = list(num_set)
+num_list.sort()
 num_dict = {}
 for i in range(len(num_list)):
-  num_dict[numlist[i]] = i
+  num_dict[num_list[i]] = i
 
 for i in numbers:
-  print(num_dict[i],end='')
+  print(num_dict[i],end=' ')
   
